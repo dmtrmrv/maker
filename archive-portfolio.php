1bc68e4a23b2168a350e5b5ce4f1ff0c26ae9155
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2015-09-03 13:23:55 +0300

    Minor bug fixes.

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -15,7 +15,7 @@ get_header(); ?>
 					
 					<?php while ( have_posts() ) : the_post(); ?>
 
-						<?php get_template_part( 'template-parts/content', 'portfolio' ); ?>
+						<?php get_template_part( 'template-parts/content', 'portfolio-toolkit' ); ?>
 
 					<?php endwhile; ?>
 
