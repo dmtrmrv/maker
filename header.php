8cd5ca245654b7eb4d5e7e322962543181fd00ac
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2015-09-23 09:05:44 +0300

    Updated styles for site container.

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
 </head>
 
@@ -39,4 +38,3 @@
 			</nav><!-- #site-navigation -->
 		</div><!-- .column -->
 	</header><!-- #masthead -->
-
