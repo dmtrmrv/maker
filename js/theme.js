fb655549108924b4cbe99c67bee2a41820d12b01
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2016-03-29 10:00:16 +0300

    Update pagination
    Add a small script that checks if the user is on the last page and add
    a 'last-page' class to the 'nav-links' div. This will fix an issue when
    the faux pagination button is visible on all pages when custom color is
    set as a text color.
    
    Update CSS to reflect the change described above and hide the faux
    pgination buttom from all pages but last.

--- js/theme.js
+++ js/theme.js
@@ -211,3 +211,17 @@ jQuery( document ).ready( function( $ ) {
 	}
 	makerFitvids();
 } );
+
+/**
+ * Adds a 'last-page' class to 'nav-links' if necessary.
+ */
+( function() {
+	// Try to get the pagination.
+	var nav = document.querySelector( '.nav-links' );
+
+	// Look for 'next' class within 'nav-links'.
+	if ( nav && ! nav.querySelector( '.next' ) ) {
+		// Add 'last-page' class to 'nav-links' if not found.
+		nav.className += ' last-page';
+	}
+} )();
