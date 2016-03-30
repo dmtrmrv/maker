a3fe5d462ed77e5411d501751451b664207a4c54
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2016-03-30 20:09:47 +0300

    Improve main navigation
    Make submenus open when navigating with tabs.

--- assets/js/src/navigation.js
+++ assets/js/src/navigation.js
@@ -1,22 +1,23 @@
 /**
  * navigation.js
  *
- * Handles toggling the navigation menu for small screens.
+ * Handles toggling the navigation menu for small screens
  */
+
 ( function() {
-	var container, button, menu;
+	var container, button, menu, links, subMenus;
 
 	container = document.getElementById( 'site-navigation' );
 	if ( ! container ) {
 		return;
 	}
 
+	menu = container.getElementsByTagName( 'ul' )[0];
+
 	button = document.getElementById( 'site-navigation-toggle' );
-	if ( ! button ) {
+	if ( 'undefined' === typeof button ) {
 		return;
 	}
-
-	menu = container.getElementsByTagName( 'ul' )[0];
 
 	// Hide menu toggle button if menu is empty and return early.
 	if ( 'undefined' === typeof menu ) {
@@ -25,7 +26,6 @@
 	}
 
 	menu.setAttribute( 'aria-expanded', 'false' );
-
 	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
 		menu.className += ' nav-menu';
 	}
@@ -37,8 +37,45 @@
 			menu.setAttribute( 'aria-expanded', 'false' );
 		} else {
 			container.className += ' toggled';
-			button.className    += ' toggled';
+			button.className += ' toggled';
 			menu.setAttribute( 'aria-expanded', 'true' );
 		}
 	};
+
+	// Get all the link elements within the menu.
+	links    = menu.getElementsByTagName( 'a' );
+	subMenus = menu.getElementsByTagName( 'ul' );
+
+	// Set menu items with submenus to aria-haspopup="true".
+	for ( var i = 0, len = subMenus.length; i < len; i++ ) {
+		subMenus[i].parentNode.setAttribute( 'aria-haspopup', 'true' );
+	}
+
+	// Each time a menu link is focused or blurred, toggle focus.
+	for ( i = 0, len = links.length; i < len; i++ ) {
+		links[i].addEventListener( 'focus', toggleFocus, true );
+		links[i].addEventListener( 'blur', toggleFocus, true );
+	}
+
+	/**
+	 * Sets or removes .focus class on an element.
+	 */
+	function toggleFocus() {
+		var self = this;
+
+		// Move up through the ancestors of the current link until we hit .nav-menu.
+		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
+
+			// On li elements toggle the class .focus.
+			if ( 'li' === self.tagName.toLowerCase() ) {
+				if ( -1 !== self.className.indexOf( 'focus' ) ) {
+					self.className = self.className.replace( ' focus', '' );
+				} else {
+					self.className += ' focus';
+				}
+			}
+
+			self = self.parentElement;
+		}
+	}
 } )();
