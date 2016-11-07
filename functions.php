e8edbadc524003a48d079499410c38493cb16125
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2016-11-07 17:24:09 +0300

    Bump up the verison number

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 /**
  * The current version of the theme.
  */
-define( 'MAKER_VERSION', '0.3.2' );
+define( 'MAKER_VERSION', '0.3.3' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
